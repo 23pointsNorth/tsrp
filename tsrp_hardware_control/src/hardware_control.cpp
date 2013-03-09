e5ea2cf5703323197db839e340ce03f3669c82d9
Daniel Angelov <dani_angelov@yahoo.com>
2013-03-09 22:52:42 +0000

    Added hardware control package.

--- tsrp_hardware_control/src/hardware_control.cpp
+++ tsrp_hardware_control/src/hardware_control.cpp
@@ -2,16 +2,16 @@
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "motor_control");
+	ros::init(argc, argv, "hardware_control");
 	if (!ros::master::check())
 	{
-		ROS_ERROR("[MOTOR_CONTROL] Cannot detect ROS master!");
+		ROS_ERROR("[HARDWARE_CONTROL] Cannot detect ROS master!");
 		return 1;
 	}
-	ROS_INFO("[MOTOR_CONTROL] Subscribing and Advertising.");
+	ROS_INFO("Subscribing and Advertising.");
 	ros::NodeHandle* node = new ros::NodeHandle();	
 
-	ros::Rate motor_refresh(200); //200Hz
+	ros::Rate refresh(20); //20Hz
 
 	while(ros::ok())
 	{
