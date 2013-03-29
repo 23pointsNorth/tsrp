769e271dcc83825eed26e608faae6926cb5a4437
Daniel Angelov <dani_angelov@yahoo.com>
2013-03-29 20:10:38 +0200

    Hardware control now a ROS library.

---
+++ tsrp_hardware_control/src/hardware_control.cpp
@@ -0,0 +1,25 @@
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "hardware_control");
+	if (!ros::master::check())
+	{
+		ROS_ERROR("[HARDWARE_CONTROL] Cannot detect ROS master!");
+		return 1;
+	}
+	ROS_INFO("Subscribing and Advertising.");
+	ros::NodeHandle* node = new ros::NodeHandle();	
+
+	ros::Rate refresh(20); //20Hz
+
+	while(ros::ok())
+	{
+		ros::spinOnce();
+
+		refresh.sleep();
+	}
+	
+	ros::shutdown();
+	return 0;
+}
